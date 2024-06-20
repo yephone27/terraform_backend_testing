4cf524d869b9c55b26052e999be1b939444701fe
yephone27 <yephoneshein27@gmail.com>
2024-06-20 10:50:28 +0630

    updated versions.tf

--- versions.tf
+++ versions.tf
@@ -11,7 +11,7 @@ provider "aws" {
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = var.aws-profile
-  alias                    = "ypsalias"
+  # alias                    = "ypsalias"
   region                   = var.aws-prod-yps-region
 }
 
