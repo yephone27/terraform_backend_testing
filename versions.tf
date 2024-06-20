393a247060caa82bc274a3e5e7f4420eaeb39b06
yephone27 <yephoneshein27@gmail.com>
2024-06-20 10:43:33 +0630

    updated versions.tf

--- versions.tf
+++ versions.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = var.aws-profile
   alias                    = "ypsalias"
   region                   = var.aws-prod-yps-region
